36819b10ffac95c530bda5435565001ddacbf379
bardellis <santiago.bardelli@gmail.Com>
2024-06-05 00:23:16 +0200

    added customer_transactions table and performed 2 queries

--- semana 4.sql
+++ semana 4.sql
@@ -140,15 +140,32 @@ FROM RankedRows r
 JOIN MedianRowNum m
 ON r.region = m.region AND r.row_num = m.median_row_num;
 
--- What is the number of nodes per region?
--- How many customers are allocated to each region?
--- How many days on average are customers reallocated to a different node?
--- What is the median, 80th and 95th percentile for this same reallocation days metric for each region?
-
 
 -- B. Customer Transactions
 -- What is the unique count and total amount for each transaction type?
+select count(*) as deposits, sum(txn_amount) as amounts
+FROM customer_transactions;
+
 -- What is the average total historical deposit counts and amounts for all customers?
+select count(*) as deposits, sum(txn_amount_val) as amounts 
+FROM (select 
+	c.customer_id,
+	c.start_date, 
+    c.end_date, 
+    r.region_name,
+	case when txn_date >=start_date and txn_date <= end_date
+    then txn_date 
+    else null 
+    end as txn_date_val,
+   	case when txn_date >=start_date and txn_date <= end_date
+    then txn_amount 
+    else null 
+    end as txn_amount_val
+FROM customer_nodes AS c
+JOIN regions AS r ON c.region_id = r.region_id
+JOIN customer_transactions as t on c.customer_id= t.customer_id) subquery
+where txn_date_val is not null;
+
 -- For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
 -- What is the closing balance for each customer at the end of the month?
 -- What is the percentage of customers who increase their closing balance by more than 5%?
