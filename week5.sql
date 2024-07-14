e13953d635277c6fdeb6f3002b93eaa260c3cca7
bardellis <santiago.bardelli@gmail.Com>
2024-07-15 00:14:46 +0200

    finished points two and three second groups of question

--- week5.sql
+++ week5.sql
@@ -1,24 +1,20 @@
 /*Case Study Questions
 The following case study questions require some data cleaning steps before we start to unpack Danny’s key business questions in more depth.*/
 
-CREATE SCHEMA data_mart;
-USE data_mart;
+	CREATE SCHEMA data_mart;
+	USE data_mart;
 
-DROP TABLE IF EXISTS data_mart.weekly_sales;
+	DROP TABLE IF EXISTS data_mart.weekly_sales;
 
-CREATE TABLE data_mart.weekly_sales (
-  week_date VARCHAR(7),
-  region VARCHAR(13),
-  platform VARCHAR(7),
-  segment VARCHAR(4),
-  customer_type VARCHAR(8),
-  transactions INTEGER,
-  sales INTEGER
-);
-
--- INSERT INTO data_mart.weekly_sales (week_date, region, platform, segment, customer_type, transactions, sales) VALUES
-
-USE data_mart;
+	CREATE TABLE data_mart.weekly_sales (
+	  week_date VARCHAR(7),
+	  region VARCHAR(13),
+	  platform VARCHAR(7),
+	  segment VARCHAR(4),
+	  customer_type VARCHAR(8),
+	  transactions INTEGER,
+	  sales INTEGER
+	);
 
 -- 1. Data Cleansing Steps
 /*In a single query, perform the following operations and generate a new table in the data_mart schema named clean_weekly_sales:
@@ -32,74 +28,77 @@ Add a new column called age_band after the original segment column using the fol
 -- 2 Middle Aged
 -- 3 or 4 Retirees
 
-DROP TABLE IF EXISTS data_mart.clean_weekly_sales;
+	DROP TABLE IF EXISTS data_mart.clean_weekly_sales;
+	
+    CREATE TABLE clean_weekly_sales (
+		week_date VARCHAR(7) NOT NULL,
+		date_format DATE,
+		segment VARCHAR(4), -- Asumiendo que segment existe en la tabla original
+		day_number INT,
+		month_number INT,
+		year_number INT,
+		age_band VARCHAR(20),
+		demographic VARCHAR(20),
+		transactions int,
+		sales int,
+		avg_transaction int,
+        platform VARCHAR(7),
+	);
 
-CREATE TABLE clean_weekly_sales (
-    week_date VARCHAR(7) NOT NULL,
-    date_format DATE,
-    segment VARCHAR(4), -- Asumiendo que segment existe en la tabla original
-    day_number INT,
-    month_number INT,
-    year_number INT,
-    age_band VARCHAR(20),
-    demographic VARCHAR(20),
-    transactions int,
-    sales int,
-    avg_transaction int
-);
+	INSERT INTO clean_weekly_sales (week_date, segment, sales, transactions, platform)
+	SELECT week_date, segment, sales, transactions, platform
+	FROM weekly_sales;
 
-INSERT INTO clean_weekly_sales (week_date, segment, sales, transactions)
-SELECT week_date, segment, sales, transactions
-FROM weekly_sales;
-
+-- ------------------------------------------------------------
 -- Agregar day_number, month_number, year_number
-UPDATE clean_weekly_sales
-SET 
-    day_number = CAST(SUBSTRING_INDEX(week_date, '/', 1) AS UNSIGNED),
-    month_number = CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(week_date, '/', 2), '/', -1) AS UNSIGNED),
-    year_number = CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(week_date, '/', -1), '/', 1) AS UNSIGNED);
+	UPDATE clean_weekly_sales
+	SET 
+		day_number = CAST(SUBSTRING_INDEX(week_date, '/', 1) AS UNSIGNED),
+		month_number = CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(week_date, '/', 2), '/', -1) AS UNSIGNED),
+		year_number = CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(week_date, '/', -1), '/', 1) AS UNSIGNED);
 
-UPDATE clean_weekly_sales
-SET date_format = STR_TO_DATE(CONCAT(year_number, '-', LPAD(month_number, 2, '00'), '-', LPAD(day_number, 2, '00')), '%Y-%m-%d')
-WHERE year_number IS NOT NULL AND month_number IS NOT NULL AND day_number IS NOT NULL;
+	UPDATE clean_weekly_sales
+	SET date_format = STR_TO_DATE(CONCAT(year_number, '-', LPAD(month_number, 2, '00'), '-', LPAD(day_number, 2, '00')), '%Y-%m-%d')
+	WHERE year_number IS NOT NULL AND month_number IS NOT NULL AND day_number IS NOT NULL;
 
--- Convertir week_date a formato DATE si es necesario
-UPDATE clean_weekly_sales
-SET date_format = STR_TO_DATE(CONCAT('01/', week_date), '%d/%m/%Y');
+	-- Convertir week_date a formato DATE si es necesario
+	UPDATE clean_weekly_sales
+	SET date_format = STR_TO_DATE(CONCAT('01/', week_date), '%d/%m/%Y');
 
+-- ------------------------------------------------------------
 -- Mapear segment a age_band
-UPDATE clean_weekly_sales
-SET age_band =
-    CASE
-        WHEN segment LIKE '%1' THEN 'Young Adults'  
-        WHEN segment LIKE '%2' THEN 'Middle Aged'  
-        WHEN segment LIKE '%3' THEN 'Retirees'     
-        WHEN segment LIKE '%4' THEN 'Retirees'     
-        ELSE 'Unknown'
-    END;
+	UPDATE clean_weekly_sales
+	SET age_band =
+		CASE
+			WHEN segment LIKE '%1' THEN 'Young Adults'  
+			WHEN segment LIKE '%2' THEN 'Middle Aged'  
+			WHEN segment LIKE '%3' THEN 'Retirees'     
+			WHEN segment LIKE '%4' THEN 'Retirees'     
+			ELSE 'Unknown'
+		END;
 
+-- ------------------------------------------------------------
 -- Add a new demographic column using the following mapping for the first letter in the segment values:
--- segment demographic
--- C Couples
--- F Families
+-- segment demographic-- C Couples-- F Families
+	UPDATE clean_weekly_sales
+	SET demographic =
+		CASE
+			WHEN segment LIKE 'C%' THEN 'Couples'  
+			WHEN segment LIKE 'F%' THEN 'Families'  
+			ELSE 'Unknown'
+		END;
 
-UPDATE clean_weekly_sales
-SET demographic =
-    CASE
-        WHEN segment LIKE 'C%' THEN 'Couples'  
-        WHEN segment LIKE 'F%' THEN 'Families'  
-        ELSE 'Unknown'
-    END;
-
+-- ------------------------------------------------------------
 -- /*Ensure all null string values with an "unknown" string value in the original segment column as well as the new age_band and demographic columns
-select segment, age_band, demographic, count(*)
-FROM clean_weekly_sales
-Where segment = 'null'
-GROUP BY segment, age_band, demographic;
+	select segment, age_band, demographic, count(*)
+	FROM clean_weekly_sales
+	Where segment = 'null'
+	GROUP BY segment, age_band, demographic;
 
+-- ------------------------------------------------------------
 -- Generate a new avg_transaction column as the sales value divided by transactions rounded to 2 decimal places for each record*/
-UPDATE clean_weekly_sales
-SET avg_transaction = ROUND(sales / transactions, 2);
+	UPDATE clean_weekly_sales
+	SET avg_transaction = ROUND(sales / transactions, 2);
 
 -- 2. Data Exploration
 /*What day of the week is used for each week_date value?
@@ -113,19 +112,139 @@ Which age_band and demographic values contribute the most to Retail sales?
 Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? 
 If not - how would you calculate it instead?*/
 
+-- ------------------------------------------------------------
+-- What day of the week is used for each week_date value?
+	ALTER TABLE clean_weekly_sales
+	ADD COLUMN week_day VARCHAR(20);
+	
+    UPDATE clean_weekly_sales
+	SET week_day = DAYNAME(date_format);
+	
+    select week_day, count(*)
+	from clean_weekly_sales
+	group by week_day; -- all dates are mondays 
 
-select * from clean_weekly_sales;
+-- ------------------------------------------------------------
+-- What range of week numbers are missing from the dataset?
+		ALTER TABLE clean_weekly_sales
+		ADD COLUMN week_number VARCHAR(20);
 
-ALTER TABLE clean_weekly_sales
-ADD COLUMN week_day VARCHAR(20);
+		UPDATE clean_weekly_sales
+		SET week_number = week(date_format);
+		select week_number, count(*)
+		from clean_weekly_sales
+		group by week_number
+		order by week_number asc;
 
-UPDATE clean_weekly_sales
-SET week_day = DAYNAME(date_format);
+-- ------------------------------------------------------------ 
+-- What range of week numbers are missing from the dataset?
+		WITH RECURSIVE Weeks AS (
+			SELECT 1 AS week_num
+			UNION ALL
+			SELECT week_num + 1
+			FROM Weeks
+			WHERE week_num < 52)
+		SELECT w.week_num AS missing_week_number
+		FROM Weeks w
+		LEFT JOIN (
+			SELECT DISTINCT week_number
+			FROM clean_weekly_sales
+		) existing_weeks ON w.week_num = existing_weeks.week_number
+		WHERE existing_weeks.week_number IS NULL
+		ORDER BY missing_week_number;
 
-select week_day, count(*)
-from clean_weekly_sales
-group by week_day; -- all dates are mondays 
+-- -----------------------------------------------------------------------
+-- How many total transactions were there for each year in the dataset?
+		select year_number, count(*)
+		from clean_weekly_sales
+		group by year_number
+		order by year_number asc;
+		-- 18	5698
+		-- 19	5708
+		-- 20	5711
 
+-- -------------------------------------------------------------
+-- What is the total count of transactions for each platform
+		select platform, count(*) as quantity
+		from weekly_sales
+		group by platform
+		order by quantity asc;
+		-- Shopify	8549
+		-- Retail	8568
+
+-- -----------------------------------------------------------------------
+-- What is the percentage of sales for Retail vs Shopify for each month?
+        select
+			year_number, month_number, 
+			max(case when platform = 'Retail' then percentage_sales else 0 end) as '%Retail',
+			max(case when platform = 'Shopify' then percentage_sales else 0 end) as '%Shopify'
+        from(
+        SELECT
+			s.month_number,
+			s.year_number,
+			s.platform,
+			SUM(s.sales) AS platform_sales,
+			t.total_sales_all_platforms,
+			ROUND((SUM(s.sales) / t.total_sales_all_platforms) * 100, 2) AS percentage_sales
+		FROM
+			clean_weekly_sales s
+		JOIN (
+			-- Subquery to get total sales for each month
+			SELECT
+				month_number,
+				year_number,
+				SUM(sales) AS total_sales_all_platforms
+			FROM
+				clean_weekly_sales
+			GROUP BY
+				month_number, year_number
+		) t ON s.month_number = t.month_number AND s.year_number = t.year_number
+		GROUP BY
+			s.month_number, s.year_number, s.platform, t.total_sales_all_platforms
+		ORDER BY
+			s.year_number, s.month_number, s.platform) as subquery
+            GROUP BY
+			year_number, month_number 
+			ORDER BY
+			year_number, month_number;
+			-- Y	M	Retail  Shopify
+			-- 18	3	97.92	2.08
+			-- 18	4	97.93	2.07
+			-- 18	5	97.73	2.27
+			-- 18	6	97.76	2.24
+			-- 18	7	97.75	2.25
+			-- 18	8	97.71	2.29
+			-- 18	9	97.68	2.32
+			-- 19	3	97.71	2.29
+			-- 19	4	97.80	2.20
+			-- 19	5	97.52	2.48
+			-- 19	6	97.42	2.58
+			-- 19	7	97.35	2.65
+			-- 19	8	97.21	2.79
+			-- 19	9	97.09	2.91
+			-- 20	3	97.30	2.70
+			-- 20	4	96.96	3.04
+			-- 20	5	96.71	3.29
+			-- 20	6	96.80	3.20
+			-- 20	7	96.67	3.33
+			-- 20	8	96.51	3.49
+
+-- --------------------------------------------------------------------------------
+-- What is the percentage of sales by demographic for each year in the dataset?
+
+-- --------------------------------------------------------------------------------
+-- Which age_band and demographic values contribute the most to Retail sales?
+
+-- ------------------------------------------------------------------------------------------------------------------
+-- Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? 
+
+-- ---------------------------------------------------- 
+-- If not - how would you calculate it instead?*/
+
+	USE data_mart;
+	select * from clean_weekly_sales;
+	select * from weekly_sales;
+    
 -- 3. Before & After Analysis
 /* This technique is usually used when we inspect an important event and want to inspect the impact before and after a certain point in time.
 Taking the week_date value of 2020-06-15 as the baseline week where the Data Mart sustainable packaging changes came into effect.
